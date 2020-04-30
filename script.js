23707c12857f31b42367014a5a80ecb245520162
Studio Breuil <64478385+patricebreuil@users.noreply.github.com>
2020-04-30 16:43:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-<script>
+
 n = new Date();
 y = n.getFullYear();
 m = n.getMonth() + 1;
 d = n.getDate();
 document.getElementById("date").innerHTML = d + "." + m + "." + y;
-</script>
- <script>
+
 function startDate() {
 var n = new Date();
 var y = n.getFullYear();
@@ -14,8 +13,7 @@ var d = n.getDate();
 document.getElementById("date").innerHTML =
   d + "." + m + "." + y;
 }
-</script>
-<script>
+
 function startTime() {
   var today = new Date();
   var h = today.getHours();
@@ -31,4 +29,4 @@ function checkTime(i) {
   if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
   return i;
 }
-</script>
+
