fc4a934872effd162c3738346cdd4f66c518769c
Studio Breuil <64478385+patricebreuil@users.noreply.github.com>
2020-04-30 11:03:46 +0100

    Adding time function

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
+<script>
 n = new Date();
 y = n.getFullYear();
 m = n.getMonth() + 1;
 d = n.getDate();
 document.getElementById("date").innerHTML = d + "." + m + "." + y;
+</script>
+ <script>
+function startDate() {
+var n = new Date();
+var y = n.getFullYear();
+var m = n.getMonth() + 1;
+var d = n.getDate();
+document.getElementById("date").innerHTML =
+  d + "." + m + "." + y;
+}
+</script>
+<script>
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('txt').innerHTML =
+  h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
+</script>
